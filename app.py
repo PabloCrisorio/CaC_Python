87067982f346aaddfaef8c66e0c62e5db136f927
PabloCrisorio <110118641+PabloCrisorio@users.noreply.github.com>
2024-07-05 12:57:07 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+
+
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:1234@localhost:3306/cacgrupo19'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://pcrisorio:Matamalas11**@pcrisorio.mysql.pythonanywhere-services.com/pcrisorio$GuitarShop'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
 
-
-# Definir la tabla 
+ #Definir la tabla
 class Producto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(50))
     precio=db.Column(db.Integer)
-    tipo=db.Coolumn(db.String(50))
+    tipo=db.Column(db.String(50))
     stock=db.Column(db.Integer)
     imagen=db.Column(db.String(400))
 
@@ -27,7 +34,7 @@ class Producto(db.Model):
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(50))
-    usuario=db.Coolumn(db.String(50))
+    usuario=db.Column(db.String(50))
     password=db.Column(db.String(50))
 
     def __init__(self,nombre,usuario,password):   #crea el  constructor de la clase
@@ -40,9 +47,9 @@ with app.app_context():
     db.create_all()
 @app.route("/")
 def index():
-    return f'App GuitarShop'
+    return 'GuitarShop API server'
 
-@app.route("/registro_producto", methods=['POST']) 
+@app.route("/registro_producto", methods=['POST'])
 def registro_producto():
     # {"nombre": "Felipe", ...} -> input tiene el atributo name="nombre"
     nombre_recibido = request.json["nombre"]
@@ -54,19 +61,19 @@ def registro_producto():
     db.session.add(nuevo_registro)
     db.session.commit()
     return "Solicitud de post recibida"
-    
-@app.route("/registro_usuario", methods=['POST']) 
+
+@app.route("/registro_usuario", methods=['POST'])
 def registro_usuario():
     nombre_recibido = request.json["nombre"]
     usuario=request.json['usuario']
     password=request.json['password']
-    nuevo_registro = Producto(nombre=nombre_recibido,usuario=usuario,password=password)
+    nuevo_registro = Usuario(nombre=nombre_recibido,usuario=usuario,password=password)
     db.session.add(nuevo_registro)
     db.session.commit()
     return "Solicitud de post recibida"
 
 @app.route("/usuarios",  methods=['GET'])
-def productos():
+def usuarios():
     all_registros = Usuario.query.all()
     data_serializada = []
     for objeto in all_registros:
@@ -95,13 +102,13 @@ def update(id):
     imagen=request.json['imagen']
     producto.nombre=nombre
     producto.precio=precio
+    producto.tipo=tipo
     producto.stock=stock
     producto.imagen=imagen
     db.session.commit()
     data_serializada = [{"id":producto.id, "nombre":producto.nombre, "precio":producto.precio, "tipo":producto.tipo, "stock":producto.stock, "imagen":producto.imagen}]
     return jsonify(data_serializada)
 
-   
 @app.route('/borrar/<id>', methods=['DELETE'])
 def borrar(id):
     producto = Producto.query.get(id)
@@ -109,8 +116,3 @@ def borrar(id):
     db.session.commit()
     data_serializada = [{"id":producto.id, "nombre":producto.nombre, "precio":producto.precio, "tipo":producto.tipo, "stock":producto.stock, "imagen":producto.imagen}]
     return jsonify(data_serializada)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
